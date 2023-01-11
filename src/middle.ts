b2d1d2657efd0c2175a9049694a8f776eb46fd3e
taldanai1995 <taldanai@icloud.com>
2023-01-11 15:58:53 +0200

    Post fetch more examples

--- src/middle.ts
+++ src/middle.ts
@@ -5,5 +5,21 @@ fetch(`${local}/data`,{
         'Content-Type': 'application/json'
     },
     body: JSON.stringify({data: "Yellow"})
-}).then((res) => res.text()).then(result => console.log(result));
+}).then((res) => res.text()).then(answer => console.log(answer));
 // prints Yellow
+
+fetch(`${local}/data`,{
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({data: "Yellow"})
+}).then((res) => console.log(res));
+
+fetch(`${local}/data`,{
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({data: "Yellow"})
+}).then((res) => console.log(typeof(res)));
