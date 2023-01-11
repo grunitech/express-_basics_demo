b24bf5da7343bed9b2f1f7b58f9c075a4470a394
taldanai1995 <taldanai@icloud.com>
2023-01-11 16:05:05 +0200

    Post name global example

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,8 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 
+let nameOne = "";
+const database = [];
 app.get("/",(req:Request, res:Response)=> {
     res.send("Hello World")
 })
@@ -22,6 +24,13 @@ app.get("/oneplusone",(req:Request, res:Response)=> {
 app.get("/hello/:name",(req:Request, res:Response)=> {
     const name = req.params.name; 
     res.send(`Hello ${name}`)
+})
+
+app.get("/hello/edit/:name",(req:Request, res:Response)=> {
+    console.log(nameOne);
+    nameOne = req.params.name;
+    console.log(nameOne); 
+    res.send(`Hello ${nameOne}`);
 })
 
 app.get("/hello/upper/:name",(req:Request, res:Response)=> {
