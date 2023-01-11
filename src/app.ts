0d6343ca684f2b92a054c439f1b5bc490b7c8d64
taldanai1995 <taldanai@icloud.com>
2023-01-11 23:00:53 +0200

    added examples and documentation 1

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,82 @@
+// imports from modules
 import express from 'express'
 import {Request, Response} from 'express'
-import cors from 'cors';
 import bodyParser from 'body-parser';
+import cors from 'cors';
+
+// import from files
 import {User} from './User';
 import {Product} from './Product';
 
+// constants for app
 const app = express();
 const port = 3001;
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended : true}));
+// app uses
+app.use(cors()); // used for communications with other apps
+app.use(bodyParser.json()); // allowing the body to be parsed in my app
+app.use(bodyParser.urlencoded({extended : true})); // allowing pass query params in POST requests
 
+// mock database
+// version 1 -> Only name
 let nameOne = "";
+
+// version 2 -> Users and Products 
 const users: User[] = [];
 const products: Product[] = [];
 
-
+// App routes
+// home page GET method example 1
 app.get("/",(req:Request, res:Response)=> {
     res.send("Hello World")
 })
 
+// Add second router calculation GET method example 2
 app.get("/oneplusone",(req:Request, res:Response)=> {
     res.send(`${1+1}`)
 })
 
+// GET + Query params example 1 
 app.get("/hello/:name",(req:Request, res:Response)=> {
     const name = req.params.name; 
     res.send(`Hello ${name}`)
 })
 
+// Query params Bad example 
+// DONT UNCOMMENT :) 
+
+// app.get("/:name",(req:Request, res:Response)=> {
+//     const name = req.params.name; 
+//     res.send(`Hello ${name}`)
+// })
+
+// GET + Query params example 2 
+app.get("/hello/upper/:name",(req:Request, res:Response)=> {
+    const name = req.params.name; 
+    res.send(`Hello ${name.toUpperCase()}`)
+})
+
+// GET + Query params type example 1 
+app.get("/concat/:num1/:num2",(req:Request, res:Response)=> {
+    const num1 = req.params.num1;
+    const num2 = req.params.num2; 
+    res.send(`Result: ${num1 + num2}`)
+})
+
+// GET + Query params type example 2
+app.get("/add/:num1/:num2",(req:Request, res:Response)=> {
+    const num1 = Number(req.params.num1);
+    const num2 = Number(req.params.num2); 
+    res.send(`Result: ${num1 + num2}`)
+})
+
+// POST method example 1
+app.post("/data",(req:Request, res:Response)=> {
+    const data = req.body.data;
+    res.send(`${data}`);
+})
+
+// Mock Database method example 1
 app.get("/hello/edit/:name",(req:Request, res:Response)=> {
     console.log(nameOne);
     nameOne = req.params.name;
@@ -37,55 +84,67 @@ app.get("/hello/edit/:name",(req:Request, res:Response)=> {
     res.send(`Hello ${nameOne}`);
 })
 
-app.get("/hello/upper/:name",(req:Request, res:Response)=> {
-    const name = req.params.name; 
-    res.send(`Hello ${name.toUpperCase()}`)
-})
-
-app.get("/concat/:num1/:num2",(req:Request, res:Response)=> {
-    const num1 = req.params.num1;
-    const num2 = req.params.num2; 
-    res.send(`Result: ${num1 + num2}`)
-})
-
-app.get("/add/:num1/:num2",(req:Request, res:Response)=> {
-    const num1 = Number(req.params.num1);
-    const num2 = Number(req.params.num2); 
-    res.send(`Result: ${num1 + num2}`)
-})
-
-app.post("/data",(req:Request, res:Response)=> {
-    const data = req.body.data;
-    res.send(`${data}`);
-})
 // FROM here everything related to mock DB 
-// objType => users / products 
+// objType => Users / Products / else 
 
 app.get("/db/info",(req:Request, res:Response)=> {
-    res.send(`users: ${users} \n products:${products}`);
+    res.send(`users: ${users.map(user=>(`${user.id},${user.name}`))} 
+             products:${products.map(product=>(`${product.id},${product.name},${product.price}`))}`);
 })
+// Changed a bit from what we did in the workshop to show the data after the insert of data
+// The following function insert a new element to mock db according to the type given 
 app.post("/db/insert/:objType",(req:Request, res:Response)=> {
-    console.log(users,products)
+    console.log(users,products);
+    let message;
     const objType = req.params.objType;
     if (objType === 'Users'){
         const id:number = req.body.id;
         const name:string = req.body.name; 
         users.push(new User(id,name));
-        res.send("User was entered")
-        return;
-    }
-    if (objType === 'Products'){
+        message = "User was entered";
+    } else if (objType === 'Products'){
         const id:number = req.body.id;
         const name:string = req.body.name;
         const price:number = req.body.price;
         products.push(new Product(id,name,price));
-        res.send("Product was entered")
+        message = "Product was entered";        
+    } else{
+        message = "Nothing was added";        
+    }
+    console.log(users,products);
+    console.log();
+    res.send(message);
+
+})
+// The following function looks for a object of the sepecified objType and returns it if found else mentioned
+app.post("/db/getbyid/:objType/:oid",(req:Request, res:Response)=> {
+    console.log(users,products)
+    const objType = req.params.objType;
+    if (objType === 'Users'){
+        const id:number = Number(req.params.oid)
+        const result = users.filter((user ) => user.id === id)
+        if (result.length === 0){
+            res.send("No user was found")
+        }else{
+            res.send(`User : id: ${result[0].id}, name:${result[0].name}`)
+        }
         return;
     }
-    res.send("Nothing was added")
+    if (objType === 'Products'){
+        const id:number = Number(req.params.oid);
+        const result = products.filter((user ) => user.id === id);
+        if (result.length === 0){
+            res.send("No product was found");
+        }else{
+            res.send(`Product : id: ${result[0].id}, name:${result[0].name}, price:${result[0].price}`);
+        }
+        return;
+    }
+    res.send("Invalid");
 
 })
 
+// App listen to port
 app.listen(port, ()=>{
     console.log(`Server start at http://localhost:${port}`)
 });
--- src/middle.ts
+++ src/middle.ts
@@ -1,4 +1,21 @@
+// To use in dev tools
+// always must be first
 const local = `http://localhost:${3001}`;
+
+// Structure of fetch (url,{}) -> using the object of the request
+// fields -> HTTP method type -> will match according to the type of method specified in the App
+//                              if we use app.get() -> "GET" , if we use app.post()-> 'POST'
+
+// Body -> is passed where we usually contain most of the data in post requests.
+
+/* We want to let my App know that we are passing the data as JSON
+
+    headers: {
+        'Content-Type': 'application/json'
+    }
+
+*/
+// POST Example 1 - Test 1
 fetch(`${local}/data`,{
     method: 'POST',
     headers: {
@@ -6,41 +23,209 @@ fetch(`${local}/data`,{
     },
     body: JSON.stringify({data: "Yellow"})
 }).then((res) => res.text()).then(answer => console.log(answer));
-// prints Yellow
+// prints to console "Yellow"
+
+// POST Example 1 - Test 2
+fetch(`${local}/data`,{
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({data1: "Problem"})
+}).then((res) => res.text()).then(answer => console.log(answer));
+// prints undefined
+// since we are looking for the field "data" and here we have only the field "data1" by default we will get undefined
 
 fetch(`${local}/data`,{
     method: 'POST',
     headers: {
         'Content-Type': 'application/json'
     },
-    body: JSON.stringify({data: "Yellow"})
+    body: JSON.stringify({data: "Hello", testing: "World"})
 }).then((res) => console.log(res));
-
-fetch(`${local}/data`,{
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({data: "Yellow"})
-}).then((res) => console.log(typeof(res)));
-
+// prints "Hello" and never using the "testing" field, since we only look for the field "data"
 
 
 // DB part 2
-const newUser = {id: 1, name:"Inbal"}
+// Users
+const newUser1 = {id: 1, name: "Inbal"}
+const newUser2 = {id: 2, name: "Idan"}
+const newUser3 = {id: 3, name: "Tal"}
+const newUser4 = {id: 4, name: "Dan"}
+
+// Products 
+const newProd1 = {id: 1, name: "water", price:7}
+const newProd2 = {id: 2, name: "cola", price:10}
+const newProd3 = {id: 3, name: "hamburger", price: 20}
+const newProd4 = {id: 4, name: "milk", price:12}
+
+// Adding users succesfully
 fetch(`${local}/db/insert/Users`,{
     method: 'POST',
     headers: {
         'Content-Type': 'application/json'
     },
-    body: JSON.stringify(newUser)
+    body: JSON.stringify(newUser1)
 }).then((res) => res.text()).then(answer => console.log(answer));
 
-const newProduct = {id: 1, name:"Water", price: 5}
+fetch(`${local}/db/insert/Users`,{
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newUser2)
+}).then((res) => res.text()).then(answer => console.log(answer));
+
+fetch(`${local}/db/insert/Users`,{
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newUser3)
+}).then((res) => res.text()).then(answer => console.log(answer));
+
+fetch(`${local}/db/insert/Users`,{
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newUser4)
+}).then((res) => res.text()).then(answer => console.log(answer));
+
+// Inserting products succesfully
 fetch(`${local}/db/insert/Products`,{
     method: 'POST',
     headers: {
         'Content-Type': 'application/json'
     },
-    body: JSON.stringify(newProduct)
+    body: JSON.stringify(newProd1)
 }).then((res) => res.text()).then(answer => console.log(answer));
+
+
+fetch(`${local}/db/insert/Products`,{
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newProd2)
+}).then((res) => res.text()).then(answer => console.log(answer));
+
+fetch(`${local}/db/insert/Products`,{
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newProd3)
+}).then((res) => res.text()).then(answer => console.log(answer));
+
+fetch(`${local}/db/insert/Products`,{
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newProd4)
+}).then((res) => res.text()).then(answer => console.log(answer));
+
+// Unsuccesful inserting
+fetch(`${local}/db/insert/Product`,{
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({id: 5, name: "pasta", price:8})
+}).then((res) => res.text()).then(answer => console.log(answer));
+// Only accepts Products or Users see the URL
+
+fetch(`${local}/db/insert/User`,{
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({id: 5, name: "abc"})
+}).then((res) => res.text()).then(answer => console.log(answer));
+// Only accepts Products or Users see the URL
+
+fetch(`${local}/db/insert/haha`,{
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({id: 5, name: "abc"})
+}).then((res) => res.text()).then(answer => console.log(answer));
+// Only accepts Products or Users see the URL
+
+fetch(`${local}/db/insert/Users`,{
+    method: 'PUT',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({id: 5, name: "abc"})
+}).then((res) => res.text()).then(answer => console.log(answer));
+// Only accepts Products or Users as POST requst see the Method
+
+// get product succesfully (assuming you inserted the product with id 1)
+fetch(`${local}/db/getbyid/Products/1`,{
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+}).then((res) => res.text()).then(answer => console.log(answer));
+
+// get user succesfully (assuming you inserted the user with id 1)
+fetch(`${local}/db/getbyid/Users/1`,{
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+}).then((res) => res.text()).then(answer => console.log(answer));
+
+// get product unsuccesfully (assuming you didn't insert any product with id 7)
+fetch(`${local}/db/getbyid/Products/1`,{
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+}).then((res) => res.text()).then(answer => console.log(answer));
+
+// get user unsuccesfully (assuming you didn't insert any user with id 7)
+fetch(`${local}/db/getbyid/Users/7`,{
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+}).then((res) => res.text()).then(answer => console.log(answer));
+
+// get user unsuccesfully (assuming you didn't insert any user with id 7)
+fetch(`${local}/db/getbyid/User/7`,{
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+}).then((res) => res.text()).then(answer => console.log(answer));
+// Only accepts Products or Users as URL
+
+fetch(`${local}/db/getbyid/Users`,{
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+}).then((res) => res.text()).then(answer => console.log(answer));
+// Only accepts Products or Users, no second query param was passed
+
+fetch(`${local}/db/getbyid/Users`,{
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({id:3})
+}).then((res) => res.text()).then(answer => console.log(answer));
+// Only accepts Products or Users, even though I passed a second param 
+// it was passed from body
+
+fetch(`${local}/db/getbyid/Users/7`,{
+    method: 'PUT',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+}).then((res) => res.text()).then(answer => console.log(answer));
+// Only accepts Products or Users as POST requst see the Method
