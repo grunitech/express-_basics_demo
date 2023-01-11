316e867fd3763c12fb28442ba103d46dafb1d869
taldanai1995 <taldanai@icloud.com>
2023-01-11 14:54:28 +0200

    added query params route 2 uppercase

--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,13 @@ app.get("/hello/:name",(req:Request, res:Response)=> {
     const name = req.params.name; 
     res.send(`Hello ${name}`)
 })
+
+app.get("/hello/upper/:name",(req:Request, res:Response)=> {
+    const name = req.params.name; 
+    res.send(`Hello ${name.toUpperCase()}`)
+})
+
+
 app.listen(port, ()=>{
     console.log(`Server start at http://localhost:${port}`)
 });
