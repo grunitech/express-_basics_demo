bba8866be3e6e4a0c4c62d9396540258f0895ada
taldanai1995 <taldanai@icloud.com>
2023-01-11 14:39:30 +0200

    add base route

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import {Request, Response} from 'express'
+
+const app = express();
+const port = 3001;
+
+app.get("/",(req:Request, res:Response)=> {
+    res.send("Hello Wolrd")
+})
+
+
+app.listen(port, ()=>{
+    console.log(`Server start at http://localhost:${port}`)
+});
+
