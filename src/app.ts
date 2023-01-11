3896b09899406e3c05418e74f01c0588aa35958e
taldanai1995 <taldanai@icloud.com>
2023-01-11 14:45:47 +0200

    added one more route

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,12 @@ app.get("/",(req:Request, res:Response)=> {
     res.send("Hello World")
 })
 
-
+app.get("/oneplusone",(req:Request, res:Response)=> {
+    res.send(`${1+1}`)
+})
+app.get("/users/",(req:Request, res:Response)=> {
+    res.send(`${1+1}`)
+})
 app.listen(port, ()=>{
     console.log(`Server start at http://localhost:${port}`)
 });
